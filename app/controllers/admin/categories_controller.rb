097a6aaeb629b129e07ef388d685bff21e835e77
vshibukawa <viviane.shibukawa@gmail.com>
2019-05-16 22:04:30 +0000

    add admin authentication

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,5 @@
 class Admin::CategoriesController < ApplicationController
-
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'].to_s, password: ENV['ADMIN_PASSWORD'].to_s
   def new
     @category = Category.new
   end
