d118b0cbcce8811464910cca2ce5b2897492c490
vshibukawa <viviane.shibukawa@gmail.com>
2019-05-17 12:03:18 +0000

    add review model validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,9 @@
 class Review < ActiveRecord::Base
   belongs_to :products
   belongs_to :users
+
+  validates :product_id,  presence: true
+  validates :user_id,     presence: true
+  validates :description, presence: true
+  validates :rating,      presence: true
 end
