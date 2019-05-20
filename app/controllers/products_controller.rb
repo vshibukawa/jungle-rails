81d1f4020724be0d5fb2066657cd7dcdf99b1287
vshibukawa <viviane.shibukawa@gmail.com>
2019-05-20 20:26:53 +0000

    refact product reviews

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,13 +7,6 @@ class ProductsController < ApplicationController
   def show
     @review = Review.new
     @product = Product.find params[:id]
-    # @reviews = @product.review.all.order(created_at: :desc)
-    # @users = User.joins(:review).where(reviews: {product_id: 12}).order(id: :asc).uniq
-    # @reviews = Review.where(product_id: @product.id).order(created_at: :desc)
-    # byebug
-    # @users = User.find(@reviews) if @reviews.size
-    # filtered = @users.collect{ |user| user if user.id == 2 }
-    # filtered[0].first_name
   end
 
 
