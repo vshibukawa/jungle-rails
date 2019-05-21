710fd30b448d1836fbbccf2317053fa4beea8cf8
vshibukawa <viviane.shibukawa@gmail.com>
2019-05-21 15:56:46 +0000

    bug fix review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,7 +5,7 @@ class ReviewsController < ApplicationController
 
   def create
     @product = Product.find params[:product_id]
-    @review  = @product.review.new review_params
+    @review  = @product.reviews.new review_params
     @review.user_id = current_user.id
     @review.save
 
